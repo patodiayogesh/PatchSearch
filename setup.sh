ac2c45c21269d9eafd97a4065ef70bbf2640e379
patodiayogesh <yogesh.patodia@columbia.edu>
2022-04-07 15:02:54 -0400

    update setup
    Signed-off-by: patodiayogesh <yogesh.patodia@columbia.edu>

--- setup.sh
+++ setup.sh
@@ -2,3 +2,6 @@ conda create -n "PatchSearch" python=3.8
 eval "$(conda shell.bash hook)"
 conda activate PatchSearch
 pip install -r requirements.txt
+gdown --id 1QZV7Wu-_XpXNLl0Qv8_k22dVDG03Pjf9
+unzip Patch-Dataset.zip
+rm Patch-Dataset.zip
