132a2adb8d513a4153ef44bce1cd761dfbaaa2ab
patodiayogesh <yogesh.patodia@columbia.edu>
2022-07-13 23:14:57 -0700

    Medium

--- Evaluation/experiments.py
+++ Evaluation/experiments.py
@@ -294,9 +294,11 @@ variations = [
 
 ks = [5] #1
 db_path = 'train'
-dataset_sizes = ['small']#, 'medium']
+dataset_sizes = ['medium']#, 'medium']
 query_paths = ['train', 'eval', 'test']
-filenames = [['buggy_only', 'commit_msg']]
+filenames = [['prev_code', 'buggy_only'],
+             ['prev_code', 'commit_msg'],
+             ['buggy_only', 'commit_msg']]
 methods = ['plbart']#, 'tfidf']
 variations = []
 for dataset_size in dataset_sizes:
